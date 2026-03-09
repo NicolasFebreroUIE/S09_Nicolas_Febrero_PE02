a8a57f9879b8f51e34b02591ee2eed4d6610d29e
Nicolas Febrero Lubian <nicolas.febrero.01@uie.com>
2026-03-09 12:29:42 +0100

    EX01_P5

--- S09_Nicolas_Febrero_PE02.py
+++ S09_Nicolas_Febrero_PE02.py
@@ -7,20 +7,5 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense
-
-# Here I load the train and valid datasets from the csv files given. 
-train_data = pd.read_csv('sent_train.csv')
-valid_data = pd.read_csv('sent_valid.csv')
-
-# I separate the text and the labels for the training and validation process.
-train_texts = train_data["text"].values #This will take all the values from the text column. 
-train_labels = train_data["label"].values #This  take all the values from the label column.
-valid_texts = valid_data["text"].values #This take all the values from the tex column.
-valid_labels = valid_data["label"].values #This take all the values from the label column. (I just did this to add comments as requested, its easy, but helpful to follow the code)
-
-# Here I set the basic parameters for the text processing following the general structure of the practice sessions and theorical content online
-
-vocab_size = 10000 
-max_length = 100 # I add this big number because is the typical and because it is hard to find a tweet longer than that in one sentence...
-embedding_dim = 64 #General numbes for any text processing task, as long as it is not specified in the problem.
+import numpy as np
 
