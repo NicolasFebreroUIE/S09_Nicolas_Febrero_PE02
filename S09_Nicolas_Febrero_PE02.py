6d6656fb2b62b101e88f49e778e0c4b11386af67
Nicolas Febrero Lubian <nicolas.febrero.01@uie.com>
2026-03-09 11:59:31 +0100

    EX01_P4

--- S09_Nicolas_Febrero_PE02.py
+++ S09_Nicolas_Febrero_PE02.py
@@ -17,3 +17,10 @@ train_texts = train_data["text"].values #This will take all the values from the 
 train_labels = train_data["label"].values #This  take all the values from the label column.
 valid_texts = valid_data["text"].values #This take all the values from the tex column.
 valid_labels = valid_data["label"].values #This take all the values from the label column. (I just did this to add comments as requested, its easy, but helpful to follow the code)
+
+# Here I set the basic parameters for the text processing following the general structure of the practice sessions and theorical content online
+
+vocab_size = 10000 
+max_length = 100 # I add this big number because is the typical and because it is hard to find a tweet longer than that in one sentence...
+embedding_dim = 64 #General numbes for any text processing task, as long as it is not specified in the problem.
+
