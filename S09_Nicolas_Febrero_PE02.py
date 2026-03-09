a7b4fb9ed479fe01aa6e932bfcb2bb20ac0be2f1
Nicolas Febrero Lubian <nicolas.febrero.01@uie.com>
2026-03-09 11:42:26 +0100

    EX01_P2

--- S09_Nicolas_Febrero_PE02.py
+++ S09_Nicolas_Febrero_PE02.py
@@ -7,3 +7,7 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense
+
+# Here I load the train and valid datasets from the csv files given. 
+train_data = pd.read_csv('sent_train.csv')
+valid_data = pd.read_csv('sent_valid.csv')
